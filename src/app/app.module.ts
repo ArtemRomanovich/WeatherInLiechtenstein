1d5000d746a1b8b71afbbb4b1069c84dfdb712c9
ArtemRomanovich <a_r_shukin@mail.ru>
2018-02-26 21:35:20 +0300

    getDataWeather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { Http } from '@angular/http';
+import {MaterialModule} from './material.module';
 
 
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -16,6 +18,8 @@ import { WeatherComponent } from './weather/weather.component';
   imports: [
     BrowserModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule
 
 
   ],
---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  imports: [MatListModule,MatButtonModule,MatIconModule],
+  exports: [MatListModule,MatButtonModule,MatIconModule]
+})
+export class MaterialModule {}
