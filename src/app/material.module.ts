74b91d385922c2942c6c4b7a74dc1bfe1c0e694f
ArtemRomanovich <a_r_shukin@mail.ru>
2018-02-27 16:32:01 +0300

    addCurrentWeather&Styles

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,10 @@ import {CommonModule} from '@angular/common';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatListModule,MatButtonModule,MatIconModule],
-  exports: [MatListModule,MatButtonModule,MatIconModule]
+  imports: [MatListModule,MatButtonModule,MatIconModule,MatToolbarModule],
+  exports: [MatListModule,MatButtonModule,MatIconModule,MatToolbarModule]
 })
 export class MaterialModule {}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -12,10 +12,40 @@ export class WeatherComponent implements OnInit {
   //city: Object;
   cityes: Array<any>;
   result: any;
+  extendedWeather: Object = {
+    nameCity: '',
+    description: '',
+    icon: '' ,
+    main: '',
+    headerWind: '',
+    windSpeed: '',
+    windDegree: '',
+
+  }
   //http://api.openweathermap.org/data/2.5/group?id=3042033,3042035,3042041,3042055,3042068,3042073&units=metric&appid=f39d6aece235c5518ac1422bd081a2a7
 
   constructor(private http: Http) {
 
+  }
+
+  extWeather(event){
+    console.log(event.explicitOriginalTarget.id);
+    for(let city of this.cityes){
+      if(event.explicitOriginalTarget.id == city.id){
+        console.log(city.name);
+        this.extendedWeather = {
+          nameCity: city.name,
+          description: city.weather[0].description,
+          icon: 'http://openweathermap.org/img/w/'+ city.weather[0].icon +'.png' ,
+          main: city.weather[0].main,
+          headerWind: 'Wind',
+          windSpeed: 'Degree: '+city.wind.speed,
+          windDegree: 'Speed: '+city.wind.deg,
+
+        }
+      }
+    }
+    console.log(this.extendedWeather);
   }
 
   ngOnInit() {
