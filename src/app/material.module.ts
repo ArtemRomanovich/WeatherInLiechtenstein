326651d75fc8af1916bed89aea9d2e444b2b636a
ArtemRomanovich <a_r_shukin@mail.ru>
2018-02-27 23:25:29 +0300

    addOutputOfAdditionalData

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,9 +5,31 @@ import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
-  imports: [MatListModule,MatButtonModule,MatIconModule,MatToolbarModule],
-  exports: [MatListModule,MatButtonModule,MatIconModule,MatToolbarModule]
+  imports: [
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTableModule
+],
+  exports: [
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTableModule
+  ]
 })
 export class MaterialModule {}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map'
 })
 export class WeatherComponent implements OnInit {
   //city: Object;
-  cityes: Array<any>;
+  cityes: Array<Object>;
   result: any;
   extendedWeather: Object = {
     nameCity: '',
@@ -20,50 +20,66 @@ export class WeatherComponent implements OnInit {
     headerWind: '',
     windSpeed: '',
     windDegree: '',
-
   }
+  dayWeather: Array<Object>;
   //http://api.openweathermap.org/data/2.5/group?id=3042033,3042035,3042041,3042055,3042068,3042073&units=metric&appid=f39d6aece235c5518ac1422bd081a2a7
 
   constructor(private http: Http) {
 
   }
-
+    //api.openweathermap.org/data/2.5/group?id=3042033,3042035,3042041,3042055,3042068,3042073
   extWeather(event){
-    console.log(event.explicitOriginalTarget.id);
     for(let city of this.cityes){
       if(event.explicitOriginalTarget.id == city.id){
-        console.log(city.name);
+        this.http.get('http://api.openweathermap.org/data/2.5/forecast?id='+city.id+'&units=metric&appid=f39d6aece235c5518ac1422bd081a2a7')
+              .map(res => res.json())
+              .subscribe(data => {
+                this.dayWeather = data.list;
+                for(let day of this.dayWeather){
+                  this.extendedWeather = {
+                    nameCity: city.name,
+                    description: city.weather[0].description,
+                    icon: 'http://openweathermap.org/img/w/'+ city.weather[0].icon +'.png' ,
+                    main: city.weather[0].main,
+                    headerWind: 'Wind',
+                    windSpeed: 'Speed: '+city.wind.speed +' m/s.',
+                    nHeaderTemp: 'Temperature: ',
+                    nTemp: 'Now: ',
+                    nTempMax: 'Max: ',
+                    nTempMin: 'Min: ',
+                    nHeaderHum: 'Humidity: ',
+                    nHumidity: ' %',
+                    nHeaderGrnd: 'Ground level: ',
+                    nGrndLvl: 'hPa',
+                    nHeaderSea: 'Sea level: ',
+                    nSeaLvl: 'hPa',
+                    nHeaderWtDesc: 'Descpription: ' ,
+                    nHeaderWtSpeed: 'Wind speed: ',
+                    nWiSpeed: ' m/s.',
+
+                  }
+
+                }
+              console.log(this.dayWeather);
+
+            });
         this.extendedWeather = {
           nameCity: city.name,
           description: city.weather[0].description,
           icon: 'http://openweathermap.org/img/w/'+ city.weather[0].icon +'.png' ,
           main: city.weather[0].main,
           headerWind: 'Wind',
-          windSpeed: 'Degree: '+city.wind.speed,
-          windDegree: 'Speed: '+city.wind.deg,
+          windSpeed: 'Speed: '+city.wind.speed +' m/s.',
 
         }
       }
     }
-    console.log(this.extendedWeather);
   }
 
   ngOnInit() {
     this.http.get('http://api.openweathermap.org/data/2.5/group?id=3042033,3042035,3042041,3042055,3042068,3042073&units=metric&appid=f39d6aece235c5518ac1422bd081a2a7')
-          // Call map on the response observable to get the parsed people object
           .map(res => res.json())
-          // Subscribe to the observable to get the parsed people object and attach it to the
-          // component
           .subscribe(data => {
-          // this.result = data;
-          // for(let i = 0;i>=5;i++){
-          //   this.city = {
-          //     name: this.result.list[i].name,
-          //     temp: this.result.list[i].main.temp
-          //   };
-          //   this.cityes.push({'name': this.result.list[i].name,'temp': this.result.list[i].main.temp});
-          //
-          // }
           this.cityes = data.list;
           console.log(this.cityes);
 
